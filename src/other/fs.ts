ec17f9d4a730d822f1e2da87fb5f3f9d664f9353
fweise <public@weise-games.de>
2023-01-28 21:43:23 +0100

    fix path

--- src/other/fs.ts
+++ src/other/fs.ts
@@ -1,3 +1,3 @@
 export function createInstance<T>(path: string, className: string, ...args: any[]): T {
-    return new (require(process.env.PWD + '/dist/' + path + ".js"))[className](...args) as T;
+    return new (require(`${process.env.PWD}/${path}.js`))[className](...args) as T;
 }
