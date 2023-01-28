f4a989c04393d288c90272240edd934553557b49
fweise <public@weise-games.de>
2023-01-28 20:44:25 +0100

    implement create instance function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./commands";
 export * from "./enums";
 export * from "./models";
+export * from "./other";
 export * from "./repositories";
 export * from "./responses";
 export * from "./utils";
